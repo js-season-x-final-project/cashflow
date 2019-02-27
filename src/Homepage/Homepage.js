8b23ce8b47b3126d182506888d26259b014523f4
simeon <skgryncharov@gmail.com>
2019-02-27 21:14:53 +0200

    LIttle changes on the homepage

--- src/Homepage/Homepage.js
+++ src/Homepage/Homepage.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import classes from './Homepage.module.css'
+import Button from '../UI/Button'
+// import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
+class Homepage extends React.Component {
 
-const homepage = () => (
-    <h1>Homepage</h1>
-)
+    state={
+        newPath:'/auth'
+    }
+    replaceHash =() =>{
+        this.props.history.replace(this.state.newPath);
+    }
 
-export default homepage;
+    render(){
+        return( 
+        <div className={classes.Background}>
+            <div className={classes.MainheadingPosition}>
+                <h1 className={classes.Mainheading}>Control your money from every point </h1>
+            </div>
+            <div className={classes.AuthorizePlace}>
+                <h3>Get your cashflow controlled</h3>
+                <p>
+                    When you’re on top of your money, life is good. <br/>
+                    We help you effortlessly manage your finances in one place.
+                </p>
+                <Button buttonText="Authorize yourself" onClick={this.replaceHash}/>
+            </div>
+        </div>
+       )
+    }
+}
+
+export default Homepage;
